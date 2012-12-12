2482d385ae725390af2a3dd16f0f1b60069e51e4
Markus Klemm <markus@markusklemm.net>
2012-12-12 17:40:33 +0100

    Funktion funktioniert

--- U08.py
+++ U08.py
@@ -5,3 +5,23 @@
 #Markus Klemm WS12/13 Phy-BA
 
 
+import numpy as np
+
+FILENAME = "Bewegung.dat"
+
+f = np.loadtxt(FILENAME)
+
+#print f[:,0]
+def ZDiff(x,y):
+    """Übernimmt die Arrays x und y übergibt ein Array mit den Ableitungen"""
+    
+    xplus1 = x[1:]
+    xminus1 = x[:-1]
+    yplus1 = y[1:]
+    yminus1 = y[:-1]
+    
+    return (yplus1 - yminus1) / (xplus1 - xminus1)
+   
+print ZDiff(f[:,0],f[:,1])
+
+    
