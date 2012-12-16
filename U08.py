461ee13d202661590e41fe019ed068bd3032277b
Markus Klemm <markus@markusklemm.net>
2012-12-16 14:35:21 +0100

    Korrektur und TestDiagramm

--- U08.py
+++ U08.py
@@ -10,20 +10,34 @@ import numpy as np
 FILENAME = "Bewegung.dat"
 
 f = np.loadtxt(FILENAME)
+m = 7.257 #Masse des Körpers kg
+g = 9.81 #m/s
 
-#print f[:,0]
 def ZDiff(x,y):
     """Übernimmt die Arrays x und y übergibt ein Array mit den Ableitungen"""
     
-    xplus1 = x[1:]
-    xminus1 = x[:-1]
-    yplus1 = y[1:]
-    yminus1 = y[:-1]
+    xplus1 = x[2:]
+    xminus1 = x[:-2]
+    yplus1 = y[2:]
+    yminus1 = y[:-2]
     
     return (yplus1 - yminus1) / (xplus1 - xminus1)
    
-print ZDiff(f[:,0],f[:,1])
-print ZDiff(f[:,0],f[:,2])
+vx = ZDiff(f[:,0],f[:,1])
+vz = ZDiff(f[:,0],f[:,2])
+
+plt.xlabel("Zeit in s")
+plt.title("Bewegung eines Massepunkts")
+
+plt.plot(f[1:-1,0],vx, label="Geschwindigkeit in x")
+plt.plot(f[1:-1,0],vz, label="Geschwindigkeit in z")
+plt.legend()
+
+
+plt.show()
+
+#Ekin = 0.5*m*(vx**2 + vz**2)
+#Epot = m*g*f[:,2]
 
 
 
